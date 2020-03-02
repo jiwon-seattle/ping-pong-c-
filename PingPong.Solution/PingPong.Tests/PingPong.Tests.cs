cecccd518b22836f0c6fb4b313d57073b974651f
Student <example@example.com>
2020-03-02 09:45:42 -0800

    Add tests

---
+++ PingPong.Solution/PingPong.Tests/PingPong.Tests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PingPong;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class ClassNameTests
+  {
+    [TestMethod]
+    public void PingPongResult_ReturnPong_Pong()
+    {
+      // static void Main()
+      {
+      Assert.AreEqual("1,2,pong", yourStringNumber(3));
+      }
+    }
+  }
+}
